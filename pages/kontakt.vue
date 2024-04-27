c894c754ba743e389c862534fb1d441a6306f6aa
Marvin Deuschle <marvdeus@gmail.com>
2024-04-27 21:09:43 +0200

    contact page

---
+++ pages/kontakt.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <PageHeading>
+      Kontakt
+    </PageHeading>
+
+    <div class="flex flex-col gap-8">
+      <div class="inline-flex items-center justify-center gap-8 lg:gap-12 xl:gap-16 2xl:gap-20">
+        <p class="text-right font-bold w-1/2">
+          Bandleader:
+        </p>
+        <Contact
+          class="w-1/2"
+          email="bandleiter@bluebirdbigband.de"
+          mobile="0171 5808481"
+          name="Klaus Gehrlein"
+        />
+      </div>
+
+      <div class="inline-flex items-center justify-center gap-8 lg:gap-12 xl:gap-16 2xl:gap-20">
+        <p class="text-right font-bold w-1/2">
+          Management und Buchung:
+        </p>
+        <Contact
+          class="w-1/2"
+          email="buchung@bluebirdbigband.de"
+          name="Rudi Kolbinger"
+          telephon="06232 621915"
+        />
+      </div>
+
+      <div class="inline-flex items-center justify-center gap-8 lg:gap-12 xl:gap-16 2xl:gap-20">
+        <p class="text-right font-bold w-1/2">
+          Internet:
+        </p>
+        <Contact
+          class="w-1/2"
+          email="webadmin@bluebirdbigband.de"
+          name="Webadmin"
+        />
+      </div>
+    </div>
+  </div>
+</template>
