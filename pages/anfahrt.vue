df5279788c8491ea49f87b0185dea1ecd5621e06
Mavv3006 <marvdeus@gmail.com>
2024-05-02 13:57:12 +0200

    started with adding seo into the pages

--- pages/anfahrt.vue
+++ pages/anfahrt.vue
@@ -5,13 +5,15 @@
     <div class="flex flex-col gap-8">
       <p class="text-justify">
         Probe ist jeden Mittwoch - außer in den Schulferien (
-        <NavLink href="https://www.schulferien.org/Rheinland-Pfalz/rheinland-pfalz.html">
-          Ferientermine
-          Rheinland-Pfalz
+        <NavLink
+          href="https://www.schulferien.org/Rheinland-Pfalz/rheinland-pfalz.html"
+        >
+          Ferientermine Rheinland-Pfalz
         </NavLink>
-        ) - im Unterrichtsraum 115 im 1. OG der Musikschule der Stadt Speyer. Die <span class="font-bold">Einsteiger-Dometown-Big Band</span>
-        probt von 18:15 bis 20:00 Uhr, die <span class="font-bold">BlueBirdBigBand</span> von 20:15 bis 22:00
-        Uhr.
+        ) - im Unterrichtsraum 115 im 1. OG der Musikschule der Stadt Speyer.
+        Die <span class="font-bold">Einsteiger-Dometown-Big Band</span> probt
+        von 18:15 bis 20:00 Uhr, die
+        <span class="font-bold">BlueBirdBigBand</span> von 20:15 bis 22:00 Uhr.
       </p>
 
       <div>
@@ -31,3 +33,10 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Anfahrt',
+  ogTitle: 'Anfahrt',
+})
+</script>
